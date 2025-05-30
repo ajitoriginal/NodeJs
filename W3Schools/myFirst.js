42443824de2761f36517fd767c0780c4880db437
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-05-30 12:32:46 +0530

    used url module

--- W3Schools/myFirst.js
+++ W3Schools/myFirst.js
@@ -1,8 +1,32 @@
 const http = require('http')
 const dt = require('./myFirstModule')
+const url = require('url')
 
+//demo_basic_server
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.write('Hello World!')
+//     res.end()
+// }).listen(8080)
+
+//demo_date_time
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.write('The date and time is: ' + dt.myDateTime())
+//     res.end()
+// }).listen(8080)
+
+//demo_http_url
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.write(req.url)
+//     res.end()
+// }).listen(8080)
+
+//demo_querystring
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('The date and time is: ' + dt.myDateTime())
-    res.end()
+    const q = url.parse(req.url, true).query
+    const text = q.year + ' ' + q.month
+    res.end(text)
 }).listen(8080)
