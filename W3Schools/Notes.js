ad1890fd6cbacb81642622d9155c3a53dc87f184
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-05-30 12:01:43 +0530

    commit

---
+++ W3Schools/Notes.js
@@ -0,0 +1,13 @@
+/*
+Node.js is an open source server environment.
+Node.js allows you to run JavaScript on the server.
+Node.js uses asynchronous programming.
+Node.js can generate dynamic page content.
+Node.js can create, open, read, write, delete, and close files on the server.
+Node.js can collect form data.
+Node.js can add, delete, modify data in your database.
+
+Node.js files contains task that will be executed on certain events on the server.
+Node.js is not a programming language, it is a runtime environment.
+
+*/
