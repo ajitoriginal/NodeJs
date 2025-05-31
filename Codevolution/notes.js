5e2fc6f48690c207c562de86807fbf394b21f226
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-01 00:56:27 +0530

    node.js from codevolution

---
+++ Codevolution/notes.js
@@ -0,0 +1,15 @@
+/*
+Node.js is an open-source, cross-platform JavaScript runtime environment.
+
+Why learn Node.js
+--Build end-to-end JavaScript applications
+--Major companies like Netflix, LinkedIn, PayPal have all migrated from all other
+backend technologies to Node.js
+--Full stack development is one of the most sought out skill sets by companies
+--Huge community support
+
+Prerequisites
+--JavaScript fundamentals
+--Advanced JavaScript
+--ES2015 topics
+*/
