6e106a27ded2668e1b281d47b9b05d132fccd1af
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-03 14:28:51 +0530

    local modules

---
+++ Codevolution/index.js
@@ -0,0 +1,4 @@
+const add = require('./add')
+console.log('Hello from index.js')
+const sum = add(1,4)
+console.log('sum:', sum)
--- Codevolution/notes.js
+++ Codevolution/notes.js
@@ -12,4 +12,12 @@ Prerequisites
 --JavaScript fundamentals
 --Advanced JavaScript
 --ES2015 topics
+
+Modules
+--A module is an encapsulated and reusable chunk of code that has its own context
+--In Node.js, each file is treated as a seperate module
+--Types of modules
+    Local Modules
+    Built-in Modules
+    Third Party Modules
 */
