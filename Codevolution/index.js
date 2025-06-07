fefeae5fe8517fb72e99ce8425234b7655373dc3
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-08 02:49:31 +0530

    html response

--- Codevolution/index.js
+++ Codevolution/index.js
@@ -1,14 +1,17 @@
 const http = require('node:http')
-
+const fs = require('node:fs')
 const server = http.createServer((req, res) => {
     let superHero = {
         firstName: 'Tony',
         lastName: 'Clark'
     }
-    // res.writeHead(200, {'Content-Type': 'text/html'})
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    // res.end('Hello Ajit!')
-    res.end(JSON.stringify(superHero))
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    // res.writeHead(200, {'Content-Type': 'application/json'})
+    // const file = fs.readFileSync('./index.html', 'utf-8')
+    // fs.createReadStream('./index.html').pipe(res)
+    fs.createReadStream(__dirname + '/index.html').pipe(res)
+    // res.end(file)
+    // res.end(JSON.stringify(superHero))
 })
 
 server.listen(3000, () => {
