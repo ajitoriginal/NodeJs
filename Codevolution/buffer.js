0a321769023fec84414edde461b11ba13e737567
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-08 01:13:36 +0530

    fs module

---
+++ Codevolution/buffer.js
@@ -0,0 +1,6 @@
+const buffer = new Buffer.from("Ajit")
+
+buffer.write('Singh')
+console.log(buffer.toString())
+console.log(buffer)
+console.log(buffer.toJSON())
--- Codevolution/index.js
+++ Codevolution/index.js
@@ -1,6 +1,35 @@
-const buffer = new Buffer.from("Ajit")
+const fs = require('fs')
 
-buffer.write('Singh')
-console.log(buffer.toString())
-console.log(buffer)
-console.log(buffer.toJSON())
+console.log('First')
+const filesContent = fs.readFileSync('./file.txt', 'utf-8')
+console.log(filesContent)
+
+console.log('Second')
+
+fs.readFile('file.txt', 'utf-8', (error, data) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log('readFile',data)
+    }
+})
+
+console.log('Third')
+
+fs.writeFileSync('./greet.txt', 'Hello Ajit')
+fs.writeFile('./greet.txt', ' Hello Singh', {flag: "a"}, (err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('File written in writeFile')
+    }
+})
+
+fs.appendFile('./greet.txt', ' Hello Singh', (err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('File written in appendFile')
+    }
+})
+
