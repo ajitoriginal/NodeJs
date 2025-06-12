6bd9eff4d4201db455eed4cb67a54cbb502aa93d
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-12 17:03:05 +0530

    module caching

--- Codevolution/Revision/index.js
+++ Codevolution/Revision/index.js
@@ -1,5 +1,22 @@
-require("./batman")
-require("./superman")
+const SuperHero = require("./super-hero")
+
+const batman = new SuperHero("Batman")
+console.log(batman.getName())
+batman.setName("Bruce Wayne")
+console.log(batman.getName())
+
+const superman = new SuperHero("Superman")
+console.log(superman.getName())
+
+// const superHero = require("./super-hero")
+// console.log(superHero.getName())
+// superHero.setName("Superman")
+// console.log(superHero.getName())
+
+// const newSuperHero = require("./super-hero")
+// console.log(newSuperHero.getName())
+// require("./batman")
+// require("./superman")
 
 // const addFn = require("./add")
 
