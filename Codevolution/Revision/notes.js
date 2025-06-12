4d43e624172e4f2a9eaf20e67d32e0cea32a0742
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-12 12:52:02 +0530

    revision of node.js, introduction

---
+++ Codevolution/Revision/notes.js
@@ -0,0 +1,27 @@
+/*
+Node.js is an open-source, cross-platform JavaScript runtime environment.
+Open Source: source code is publicly available for sharing and modification.
+Cross Platform: Available for Mac, Windows and Linux
+JavaScript runtime environment: 
+
+Why learn Node.js?
+--Build end-to-end JavaScript applications
+--Many companies migrated from other backend technologies to Node.js
+--Full stack development is one of the most sought out skill sets by companies
+--Huge community support
+
+Course structure
+--Terms and concepts to understand what is Node.js
+--Modules (User defined)
+--Build-in modules
+--Node.js internals
+--npm - Node Package Manager
+--CLI Tool
+--Misc
+
+Prerequisites
+--Modern JavaScript
+    --JavaScript fundamentals crash course : https://www.youtube.com/watch?v=XIOLqoPHCJ4
+    --Advanced JavaScript crash course : https://www.youtube.com/watch?v=R9I85RhI7Cg
+    --ES2015 topics : https://www.youtube.com/watch?v=-Z0i100x0mc
+*/
