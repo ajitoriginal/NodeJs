bf5306515a12c5ed60ec6be8a201421b62b5afdb
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-06-12 17:12:41 +0530

    import export patterns

--- Codevolution/Revision/index.js
+++ Codevolution/Revision/index.js
@@ -1,12 +1,21 @@
-const SuperHero = require("./super-hero")
+const math = require("./math")
 
-const batman = new SuperHero("Batman")
-console.log(batman.getName())
-batman.setName("Bruce Wayne")
-console.log(batman.getName())
+// console.log(math.add(2, 3))
+// console.log(math.subtract(2, 3))
 
-const superman = new SuperHero("Superman")
-console.log(superman.getName())
+const {add, subtract} = math
+console.log(add(2, 3))
+console.log(subtract(2, 3))
+
+// const SuperHero = require("./super-hero")
+
+// const batman = new SuperHero("Batman")
+// console.log(batman.getName())
+// batman.setName("Bruce Wayne")
+// console.log(batman.getName())
+
+// const superman = new SuperHero("Superman")
+// console.log(superman.getName())
 
 // const superHero = require("./super-hero")
 // console.log(superHero.getName())
