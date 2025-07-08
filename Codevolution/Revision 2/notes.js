9c34dd9e81c45329680c69f716b2ac8fb49b8683
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-07-08 13:07:24 +0530

    revision

---
+++ Codevolution/Revision 2/notes.js
@@ -0,0 +1,22 @@
+/*
+Node.js is an open-source, cross-platform JavaScript runtime environment.
+OPEN SOURCE - source code is publicly available for sharing and modification.
+CROSS PLATFORM - available on Mac, Windows and Linux.
+JAVASCRIPT RUNTIME ENVIRONMENT -provides all the necessary components in order to use and a JavaScript program outside the browser.
+
+JavaScript is basically ECMAScript at its core but builds on top of that.
+A JavaScript engine is a program that executes JavaScript code.
+In 2008, Google created its own JavaScript engine called V8.
+V8 is written in C++ and can be used independently or can embedded into other C++ programs.
+That allows you to write your own C++ programs which can do everything that V8 can do and more.
+Then your C++ program can run ECMAScript and additional features that you choose to incorporate.
+For example, feature that are available in C++ but not available with JavaScript.
+
+JavaScript runtime environment is a combination of V8 engine, Web Browers API's, Queues and Event Loop.
+
+Node.js consists C++ files which form the core features and JavaScript files which expose common utilities
+and some of the C++ features(File Handeling, Input Output and Network Operations) for easier consumption.
+
+
+
+*/
