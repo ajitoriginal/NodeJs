ea1f83bc32a3e7eb3cecfa57ed782f525594d370
ajitsinghoriginal@gmail.com <ajitsinghoriginal@gmail.com>
2025-07-08 13:59:34 +0530

    revision

--- Codevolution/Revision 2/index.js
+++ Codevolution/Revision 2/index.js
@@ -1,5 +1,15 @@
-require('./batman')
-require('./superman')
+const SuperHero = require('./super-hero')
+const batman = new SuperHero('Batman')
+console.log(batman.getName())
+batman.setName('Bruce')
+console.log(batman.getName())
+
+const superman = new SuperHero('Superman')
+console.log(superman.getName())
+
+
+// require('./batman')
+// require('./superman')
 
 // const add = require('./add')
 // console.log("Hello Mr. Ajit")
