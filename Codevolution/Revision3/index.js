0e2722a66117d15923d3244cb36ff1311d326ec5
Ajit Singh <ajitsingh@Ajits-MacBook-Air.local>
2025-11-22 13:22:38 +0530

    module caching

--- Codevolution/Revision3/index.js
+++ Codevolution/Revision3/index.js
@@ -2,5 +2,25 @@
 // console.log("Hello from index.js")
 // console.log(addFn(2,3))
 
-require('./batman')
-require('./superman')
+// require('./batman')
+// require('./superman')
+// const superHero = require('./super-hero.js')
+// console.log(superHero.getName())
+// superHero.setName("Superman")
+// console.log(superHero.getName())
+
+// const newSuperHero = require('./super-hero.js') //this will be ignored by the first import
+// console.log(newSuperHero.getName())
+
+const SuperHero = require("./super-hero.js")
+const batman = new SuperHero("Batman")
+console.log(batman.getName())
+batman.setName("Bruce")
+console.log(batman.getName())
+
+const superman = new SuperHero("Superman")
+console.log(superman.getName())
+superman.setName("Clark")
+console.log(superman.getName())
+
+console.log(batman.getName())
