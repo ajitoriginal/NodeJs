2095fd59ba4a8759c2b62662467a5091e0b1f6e4
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-02 00:02:01 +0530

    learning

---
+++ Codevolution/Revision4/7-Browser vs NodeJS.js
@@ -0,0 +1,7 @@
+// You don't have the document, window and all other objects in Node.js that are provided by the browser.
+
+// We don't have all the nice APIs in the browser that Node.js provides through its modules. E.g. filesystem etc.
+
+// With Node.js, you control the environment
+
+// With a browser, you are at the mercy of what the users choose.
