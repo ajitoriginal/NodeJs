c949a3454fb70dd7c7988732f88fc49516830c98
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-01 14:32:54 +0530

    learning

---
+++ Codevolution/Revision4/4-JavaScript Runtime.js
@@ -0,0 +1,21 @@
+// An environment which provides all the necessary components in order to use and run
+// a JavaScript program
+
+// Every browser has a JavaScript Engine
+
+// A JavaScript Engine is one component in the JavaScript Runtime
+
+
+// Chrome Browser JavaScript Runtime
+// - V8 JavaScript Engine
+//   - Memory
+//   - Call Stack
+// - WEB/Browser APIs
+//   - DOM, Storage, Timers
+// - Queues
+//   - Microtask Queue
+//   - Callback/Task Queue
+// - Event Loop
+
+// Concepts so far
+// ECMAScript >> JavaScript Engine >> JavaScript Runtime
