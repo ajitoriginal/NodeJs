6c40b6340c5f8045df0788dc398088fb56f7bac4
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-01 14:19:31 +0530

    learning

---
+++ Codevolution/Revision4/3-Chrome's V8 Engine.js
@@ -0,0 +1,11 @@
+// - A JavaScript engine is a program that executes JavaScript code
+// - In 2008, Google created its own JavaScript engine called V8
+// - V8 is written in C++ and can be used independently or can be embedded into other C++ programs
+// - That allows you to write your own C++ programs which can do everything that V8 can do and more
+// - Your C++ program can run ECMAScript and additional features that you choose to incorporate
+// - For example, features that are available in C++ but not available with JavaScript
+// - Chrome's V8 engine sits at the core of Node.js
+// - Since C++ is great for lower level operations like file handeling, database connections
+//   and network operations, by embedding V8 into your own C++ program, you have the power to
+//   add all of that functionality in JavaScript
+// - The C++ program we're talking about is Node.js (However Node.js is a lot more than just a C++ program)
