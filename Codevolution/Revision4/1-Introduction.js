de128f11fed0213b9b25567f717efd9837ec06aa
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-01 13:53:57 +0530

    learning

--- Codevolution/Revision4/1-Introduction.js
+++ Codevolution/Revision4/1-Introduction.js
@@ -5,3 +5,10 @@ Node.js is an open-source, cross-platform JavaScript runtime environment.
 Open-Source : source code is publicly available for sharing and modification
 Cross-Platform : Available on Mac, Windows and Linux
 JavaScript runtime environment : 
+
+Why learn Node.js?
+
+- Build end-to-end JavaScript applications
+- A number of major companies like LinkedIn, Netflix, Paypal have all migrated to other backend technologies to Node.js
+- Full stack development is one of the most sought out skill sets by companies
+- Hugh company support
