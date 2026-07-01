a3ebff6ca04f475f9b18b058bdab451df7467752
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-01 14:07:27 +0530

    learning

---
+++ Codevolution/Revision4/2-ECMAScript.js
@@ -0,0 +1,4 @@
+// ECMA-262 is the language specification
+// ECMAScript is the language that implements ECMA-262
+// JavaScript is basically ECMAScript at its core but builds on top of that
+// Latest version : ECMAScript 2025 (also known as Edition 16 or ES16), which was officially finalized and published in June 2025
