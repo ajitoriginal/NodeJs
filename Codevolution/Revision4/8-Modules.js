fee757957c721577f9329ef7a1b44bc4e59884b6
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-02 00:07:48 +0530

    learning

---
+++ Codevolution/Revision4/8-Modules.js
@@ -0,0 +1,8 @@
+// A module is an encapsulated and reusable chunk of code that has its own context
+
+// In Node.js, each file is treated as a seperate module.
+
+// Types of Modules
+// 1. Local modules : Modules that we create in our application
+// 2. Built-in modules : Modules that Node.js ships with out of the box
+// 3. Third party modules : Modules writtend by other developers that we can use in our application
