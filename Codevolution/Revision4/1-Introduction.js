eb5dfb40b72a28c1bb9e25998931d7d603780e38
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-01 13:51:15 +0530

    learning

---
+++ Codevolution/Revision4/1-Introduction.js
@@ -0,0 +1,7 @@
+What is Node.js
+
+Node.js is an open-source, cross-platform JavaScript runtime environment.
+
+Open-Source : source code is publicly available for sharing and modification
+Cross-Platform : Available on Mac, Windows and Linux
+JavaScript runtime environment : 
