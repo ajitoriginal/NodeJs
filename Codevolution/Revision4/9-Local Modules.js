dd1efaa03deb5832757355fad0116b0127854c8b
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-02 00:31:45 +0530

    learning

---
+++ Codevolution/Revision4/9-Local Modules.js
@@ -0,0 +1,13 @@
+// In Node.js, each file is a module that is isolated by default
+
+// To load a module into another file, we use the require function
+
+// When index.js is executed, the code in the module is also executed
+
+// If the file we are requiring is a JavaScript file, we can skip specifying the extension
+// and node.js will infer it on our behalf
+
+// ==== Common JS ====
+// CommonJS is a standard that states how a module should be structured and shared
+
+// Node.js adopted CommonJS when it started out and is what you will see in code bases
