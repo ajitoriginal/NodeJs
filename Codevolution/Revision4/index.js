23169ad526de65ccb2df64d24583fdbcc6a4fd00
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-02 15:20:13 +0530

    learning

--- Codevolution/Revision4/index.js
+++ Codevolution/Revision4/index.js
@@ -1,5 +1,10 @@
 
-require("./add.js")
+const addFn = require("./add.js")
 
 console.log('Hello from index.js')
 
+const sum = addFn(2, 3)
+console.log(sum)
+
+const sum2 = addFn(3, 4)
+console.log(sum2)
