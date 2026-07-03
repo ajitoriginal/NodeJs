be8a329e6848560069759925f7d58e655e6aa333
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-03 16:04:24 +0530

    learning

--- Codevolution/Revision4/index.js
+++ Codevolution/Revision4/index.js
@@ -16,20 +16,32 @@
 // ================================================================================================
 
 // ================================= 13-Module Caching.js ===============================================================
-const SuperHero = require('./super-hero.js')
-// console.log(superHero.getName())
-const batman = new SuperHero('Batman')
-// superHero.setName('Superman')
-console.log(batman.getName())
-batman.setName('Bruce')
-console.log(batman.getName())
+// const SuperHero = require('./super-hero.js')
+// // console.log(superHero.getName())
+// const batman = new SuperHero('Batman')
+// // superHero.setName('Superman')
+// console.log(batman.getName())
+// batman.setName('Bruce')
+// console.log(batman.getName())
 
-// const newSuperHero = require('./super-hero.js')
-// superHero.setName('Batman')
-const superman = new SuperHero('Superman')
-console.log(superman.getName())
-superman.setName('Clark')
-console.log(superman.getName())
+// // const newSuperHero = require('./super-hero.js')
+// // superHero.setName('Batman')
+// const superman = new SuperHero('Superman')
+// console.log(superman.getName())
+// superman.setName('Clark')
+// console.log(superman.getName())
 
+// ================================================================================================
+
+// ================================ 14-Import Export Patterns.js ==================================
+const math = require('./math.js')
+console.log(math.add(2, 3))
+console.log(math.subtract(2, 3))
+// const mathFn = require('./math.js')
+// console.log(mathFn.add(2, 3))
+// console.log(mathFn.subtract(2, 3))
+// const {add, subtract} = math
+// console.log(add(2, 3))
+// console.log(subtract(2, 3))
 // ================================================================================================
 
