c1e504570c2ef9ab1c5d1bc84f932634b3c93e51
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-06 13:56:48 +0530

    learning

---
+++ Codevolution/Revision4/23-Character Sets and Encoding.js
@@ -0,0 +1,29 @@
+// Binary Data
+// Computers store and represent data in binary format which is a collection of 0s and 1s
+
+// Each 0 and 1 is called a binary digit or bit for short
+
+// To work with a piece of data, a computer needs to convert that data into its
+// binary representation
+
+// Character Sets
+// Character sets are predefined lists of characters represented by numbers
+
+// Popular character sets - Unicode and ASCII
+
+// Character Encoding
+// Character encoding dictates how to represent a number in a character set as binary data
+// before it can be stored in a computer
+
+// It dictates how many bits to use to represent the number
+
+// Example of a character encoding system is UTF-8
+
+// UTF-8 dictates that characters should be encoded in bytes (8 bits)
+
+// Eight 1s or 0s should be used to represent the code of any character in binary
+
+// 4 => 100 => 00000100
+// V => 86 => 01010110
+
+// Similar guidelines also exist on how images and videos should be encoded and stored in binary format
