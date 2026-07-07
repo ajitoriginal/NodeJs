97e927dc10413a7b9632743bc5cb818d7232a19d
ajitoriginal <ajitsinghoriginal@gmail.com>
2026-07-07 13:07:53 +0530

    HTTP Module

---
+++ Codevolution/Revision4/30-HTTP Module.js
@@ -0,0 +1,15 @@
+// HTTP
+// Hyper Text Transfer Protocol
+// A Protocol that defines a format for clients and servers to speak to each other
+// The client sends an HTTP request and the server responds with and HTTP response
+
+// HTTP and Node.js
+// We can create a web server using Node.js
+// Node.js has access to operating system functionality like networking
+
+// Node.js has and event loop to run task asynchronously and is perfect for creating web servers
+// that can simultaneously handle large volumes of requests
+
+// The Node.js server we create should still respect the HTTP format
+
+// The HTTP module allows creation of web servers that can transfer data over HTTP
